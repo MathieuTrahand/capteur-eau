ae45b2982328cc4a3ce09bbc792676a4efc39a37
MathieuTrahand <58786333+MathieuTrahand@users.noreply.github.com>
2021-02-04 17:27:25 +0100

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,22 @@
 let eau = 0
+let Multicolor = neopixel.create(DigitalPin.P2, 30, NeoPixelMode.RGB)
 basic.forever(function () {
     eau = pins.analogReadPin(AnalogPin.P0)
     if (eau <= 450) {
-        pins.digitalWritePin(DigitalPin.P2, 1)
+        Multicolor.showRainbow(1, pins.map(
+        eau,
+        0,
+        3823,
+        0,
+        360
+        ))
     } else {
-        pins.digitalWritePin(DigitalPin.P2, 0)
+        Multicolor.showRainbow(1, pins.map(
+        eau,
+        0,
+        50,
+        100,
+        200
+        ))
     }
 })
