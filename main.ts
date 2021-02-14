87d6fab8ac68bb59eee14222c1a683d39410672d
MathieuTrahand <58786333+MathieuTrahand@users.noreply.github.com>
2021-02-14 15:18:07 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,22 +1,18 @@
-let eau = 0
-let Multicolor = neopixel.create(DigitalPin.P2, 30, NeoPixelMode.RGB)
+input.onButtonPressed(Button.A, function () {
+    Multicolor.showRainbow(350, 127)
+})
+input.onButtonPressed(Button.B, function () {
+    let eau = 0
+    Multicolor.showRainbow(1, pins.map(
+    eau,
+    0,
+    50,
+    100,
+    200
+    ))
+})
+let Multicolor: neopixel.Strip = null
+Multicolor = neopixel.create(DigitalPin.P2, 30, NeoPixelMode.RGB)
 basic.forever(function () {
-    eau = pins.analogReadPin(AnalogPin.P0)
-    if (eau <= 450) {
-        Multicolor.showRainbow(1, pins.map(
-        eau,
-        0,
-        3823,
-        0,
-        360
-        ))
-    } else {
-        Multicolor.showRainbow(1, pins.map(
-        eau,
-        0,
-        50,
-        100,
-        200
-        ))
-    }
+	
 })
