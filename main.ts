2f4e6074cd27d77cb49c8f4f22fe513f4b906283
MathieuTrahand <58786333+MathieuTrahand@users.noreply.github.com>
2021-03-04 14:06:28 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,12 @@
 input.onButtonPressed(Button.A, function () {
     Multicolor.showRainbow(350, 127)
 })
+function TimeCalculator () {
+    if (start == 1) {
+        temps += 1
+        basic.pause(1000)
+    }
+}
 input.onButtonPressed(Button.B, function () {
     let eau = 0
     Multicolor.showRainbow(1, pins.map(
@@ -12,12 +18,10 @@ input.onButtonPressed(Button.B, function () {
     ))
 })
 let Multicolor: neopixel.Strip = null
-let temps = 0
 let start = 0
+let temps = 0
+start = 0
 Multicolor = neopixel.create(DigitalPin.P2, 30, NeoPixelMode.RGB)
 basic.forever(function () {
-    if (start == 1) {
-        temps += 1
-        basic.pause(1000)
-    }
+    TimeCalculator()
 })
