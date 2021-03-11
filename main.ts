7bd652e90604cd5fa33f3ee56c9ac3f5ec6cfe13
MathieuTrahand <58786333+MathieuTrahand@users.noreply.github.com>
2021-03-11 17:18:39 +0100

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,18 @@
 function TimeToShow () {
-	
+    if (Time > 0) {
+        Time = Time - 1
+        _4digit.show(Time)
+    } else {
+        EndFonction()
+    }
+}
+function EndFonction () {
+    _4digit.show(8888)
+    music.playMelody("G G G G G G G G ", 120)
+    basic.pause(100)
+    _4digit.clear()
+    music.stopAllSounds()
+    basic.pause(100)
 }
 function TimeCalculator () {
     if (start == 1) {
@@ -9,9 +22,14 @@ function TimeCalculator () {
 }
 let eau = 0
 let temps = 0
+let Time = 0
+let _4digit: grove.TM1637 = null
 let start = 0
 let ShowerTime = 900
 start = 0
+ShowerTime = 15
+_4digit = grove.createDisplay(DigitalPin.P2, DigitalPin.P16)
+Time = ShowerTime
 basic.forever(function () {
     eau = pins.analogReadPin(AnalogPin.P0)
     if (eau <= 450) {
