95dc690a903e4514c2fd497c1c2b0b0ed5234e51
MathieuTrahand <58786333+MathieuTrahand@users.noreply.github.com>
2021-03-11 16:48:14 +0100

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,74 +1,24 @@
-input.onButtonPressed(Button.A, function () {
-    Multicolor.showRainbow(350, 127)
-})
+function TimeToShow () {
+	
+}
 function TimeCalculator () {
     if (start == 1) {
         temps += 1
         basic.pause(1000)
     }
 }
-function LedFlashing () {
-    while (eau <= 450) {
-        range2.showColor(neopixel.rgb(255, 0, 0))
-        basic.pause(1000)
-        range2.showColor(neopixel.rgb(0, 0, 0))
-        basic.pause(1000)
-    }
-}
-function ColorGestion () {
-    Multicolor.setPixelColor(led2, couleur)
-    Multicolor.show()
-    led2 += 1
-    if (led2 == 3) {
-        couleur = neopixel.rgb(51, 204, 0)
-    }
-    if (led2 == 6) {
-        couleur = neopixel.rgb(102, 204, 0)
-    }
-    if (led2 == 9) {
-        couleur = neopixel.rgb(153, 204, 0)
-    }
-    if (led2 == 15) {
-        couleur = neopixel.rgb(204, 204, 0)
-    }
-    if (led2 == 18) {
-        couleur = neopixel.rgb(204, 153, 0)
-    }
-    if (led2 == 21) {
-        couleur = neopixel.rgb(204, 102, 0)
-    }
-    if (led2 == 24) {
-        couleur = neopixel.rgb(204, 51, 0)
-    }
-    if (led2 == 27) {
-        couleur = neopixel.rgb(204, 0, 0)
-    }
-    if (led2 == 30) {
-        LedFlashing()
-    }
-}
 let eau = 0
 let temps = 0
-let couleur = 0
-let range2: neopixel.Strip = null
-let Multicolor: neopixel.Strip = null
-let led2 = 0
 let start = 0
 let ShowerTime = 900
 start = 0
-led2 = 0
-Multicolor = neopixel.create(DigitalPin.P2, 30, NeoPixelMode.RGB)
-range2 = Multicolor.range(0, 30)
-range2.showColor(neopixel.rgb(0, 0, 0))
-led2 = 0
-couleur = neopixel.rgb(0, 255, 0)
 basic.forever(function () {
     eau = pins.analogReadPin(AnalogPin.P0)
     if (eau <= 450) {
         start = 1
         if (temps == 1) {
             temps = 0
-            ColorGestion()
+            TimeToShow()
         }
     } else {
         start = 0
