9d383ef5fdcba3481d97b7e81adea4c54ec5af3b
MathieuTrahand <58786333+MathieuTrahand@users.noreply.github.com>
2021-03-13 14:10:04 +0100

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -63,11 +63,12 @@ range = Multicolor.range(0, 30)
 range.showColor(neopixel.rgb(0, 0, 0))
 led2 = 0
 couleur = neopixel.rgb(0, 255, 0)
+serial.writeLine("entrez le shower time")
 basic.forever(function () {
     eau = pins.analogReadPin(AnalogPin.P0)
     if (eau <= 450) {
         start = 1
-        if (temps == 1) {
+        if (temps == ShowerTime / 30) {
             temps = 0
             ColorGestion()
         }
